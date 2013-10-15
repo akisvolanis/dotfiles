227eebc623ff49b452843c5b7a4498e54041ca97
Akis Volanis <c.volanis@gmail.com>
2013-10-15 12:10:14 +0300

    Update path variables and editor

--- zsh/oh-my-zsh/00_env.zsh
+++ zsh/oh-my-zsh/00_env.zsh
@@ -6,9 +6,11 @@ export PATH="/usr/local/share/npm/bin:$PATH"
 export PATH="/usr/local/share/python:$PATH"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
+export PATH="/usr/local/heroku/bin:$PATH" # heroku
+export PATH="/Applications/Adobe\ Flash\ Builder\ 4.7/sdks/4.10\(AIR\ 3.9\)/bin:$PATH" # ADT
 
 # Editor
-export EDITOR="vim"
+export EDITOR="subl -w""
 
 # Use vim for man
 export MANPAGER="col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -"
