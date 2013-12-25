febf334ac897cdb5b293cfe0abc2cc3151a79bf6
Akis Volanis <c.volanis@gmail.com>
2013-12-25 13:43:41 +0200

    Added conf.

--- zsh/oh-my-zsh/00_env.zsh
+++ zsh/oh-my-zsh/00_env.zsh
@@ -6,7 +6,7 @@ export PATH="/usr/local/share/npm/bin:$PATH"
 export PATH="/usr/local/share/python:$PATH"
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
-export PATH="/usr/local/heroku/bin:$PATH"
+#export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="/Applications/Adobe Flash Builder 4.7/sdks/4.10(AIR 3.9)/bin:$PATH"
 
 # Editor
--- zsh/oh-my-zsh/03_aliases.zsh
+++ zsh/oh-my-zsh/03_aliases.zsh
@@ -1,6 +1,7 @@
 alias l='ls -lAh'
 alias ..='cd ..' # go back one directory
 alias tc='top -o cpu' # cpu usage
+alias vm='vagrant' # vagrant
 
 # Make sudo work with aliases
 alias sudo='sudo '
