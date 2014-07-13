08444ed655623b3561255cb25684404785bfdf6e
Akis Volanis <c.volanis@gmail.com>
2014-07-13 13:27:42 +0300

    Aliases change.

--- zsh/oh-my-zsh/03_aliases.zsh
+++ zsh/oh-my-zsh/03_aliases.zsh
@@ -2,6 +2,7 @@ alias l='ls -lAh'
 alias ..='cd ..' # go back one directory
 alias tc='top -o cpu' # cpu usage
 alias vm='vagrant' # vagrant
+alias tg='tugboat' # tugboat
 
 # Make sudo work with aliases
 alias sudo='sudo '
