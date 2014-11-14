24468eb9ebc2d80754ca2cab2ca3affa3df16281
Akis <c.volanis@gmail.com>
2014-11-15 01:12:10 +0200

    Fic merge conflict.

--- zsh/oh-my-zsh/03_aliases.zsh
+++ zsh/oh-my-zsh/03_aliases.zsh
@@ -3,7 +3,7 @@ alias ..='cd ..' # go back one directory
 alias tc='top -o cpu' # cpu usage
 alias vm='vagrant' # vagrant
 alias tg='tugboat' # tugboat
-alias bower='noglob bower'
+#alias bower='noglob bower'
 
 # Make sudo work with aliases
 alias sudo='sudo '
